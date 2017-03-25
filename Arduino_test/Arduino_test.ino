86a177252459feff6a2f0db4e5b34c9f191378be
Daniel <Privorotskyd@gmail.com>
2017-03-25 19:57:34 -0400

    Test code for Motor Shield Compiled
    Test code for Motor Shield Compiled and ready to rolllllll

--- Arduino_test/Arduino_test.ino
+++ Arduino_test/Arduino_test.ino
@@ -1,20 +1,83 @@
-#include <dht.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 
-dht DHT;
+//Setup AFMS class
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
-#define DHT11_PIN 7
+//Set up all four motors with their corresponding ports
+Adafruit_DCMotor *Motor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *Motor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *Motor3 = AFMS.getMotor(3);
+Adafruit_DCMotor *Motor4 = AFMS.getMotor(4);
+
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600); 
+  Serial.println("Testing all four motors!");
+  
+  // create a connection to the Motor Shield with the default frequency 1.6KHz
+  AFMS.begin();  
 }
 
 void loop()
 {
-  float chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
-  delay(1000);
+  uint8_t i;
+  
+    Serial.println("Motor 1");
+    
+       Motor1->run(FORWARD);
+      for (i=0; i<255; i++) {
+        Motor1->setSpeed(i);  
+        delay(10);
+      }
+      for (i=255; i!=0; i--) {
+        Motor1->setSpeed(i);  
+        delay(10);
+      }
+      Motor1->run(RELEASE);
+    
+    
+    Serial.println("Motor 2");
+          
+       Motor2->run(FORWARD);
+      for (i=0; i<255; i++) {
+        Motor2->setSpeed(i);  
+        delay(10);
+      }
+      for (i=255; i!=0; i--) {
+        Motor2->setSpeed(i);  
+        delay(10);
+      }
+      Motor2->run(RELEASE);
+    
+  
+    Serial.println("Motor 3");
+          
+       Motor3->run(FORWARD);
+      for (i=0; i<255; i++) {
+        Motor3->setSpeed(i);  
+        delay(10);
+      }
+      for (i=255; i!=0; i--) {
+        Motor3->setSpeed(i);  
+        delay(10);
+      }
+      Motor3->run(RELEASE);
+    
+  
+    Serial.println("Motor 4");
+          
+       Motor4->run(FORWARD);
+      for (i=0; i<255; i++) {
+        Motor4->setSpeed(i);  
+        delay(10);
+      }
+      for (i=255; i!=0; i--) {
+        Motor4->setSpeed(i);  
+        delay(10);
+      }
+    
+    Motor4->run(RELEASE);
 }
 
