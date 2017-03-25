16809de06bad7980e4ebd748d08fcb86420662a2
Daniel <Privorotskyd@gmail.com>
2017-03-25 14:54:43 -0400

    Testing Arduino
    Verifying Arduino Uno basic funcationality, such as digital LED function

---
+++ Arduino_test/Arduino_test.ino
@@ -0,0 +1,16 @@
+int RED = 7;
+int YELLOW = 4;
+int GREEN = 5;
+int timer = 500;
+
+void setup() {
+  pinMode(RED,LOW);
+  pinMode(YELLOW,LOW);
+  pinMode(GREEN,LOW);
+}
+
+void loop() {
+  
+  digitalWrite(RED,LOW);
+  
+}
